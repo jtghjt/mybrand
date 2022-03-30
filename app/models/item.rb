0b575f75870b65822bbb1a3a3916f137ffb78cd1
jtghjt <junjunjun.t.13@gmail.com>
2022-03-30 17:49:46 +0900

    itemモデルのアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,17 @@
 class Item < ApplicationRecord
+  has_many :item_orders
+  has_many :item, through: :item_orders
+
+  has_many :favorites
+  has_many :carts
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :category_id
+    validates :color_id
+    validates :size_id
+    validates :price
+  end
 end
