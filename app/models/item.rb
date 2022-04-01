112323bfe564458f3885750869414a657aca9125
jtghjt <junjunjun.t.13@gmail.com>
2022-04-01 15:30:45 +0900

    TOPページの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,20 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  
+  # テーブルアソシエーション
   has_many :item_orders
   has_many :item, through: :item_orders
 
   has_many :favorites
   has_many :carts
+
+  # アクティブハッシュアソシエーション
+  belongs_to :category
+  belongs_to :color
+  belongs_to :size
+
+  #アクティブストレージ
+  has_one_attached :image
 
   with_options presence: true do
     validates :image
@@ -14,4 +25,12 @@ class Item < ApplicationRecord
     validates :size_id
     validates :price
   end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :color_id
+    validates :size_id
+  end
+
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+item_1 = Item.new(
+  name: "カットソー",
+  description: "ベーシックなアイテムで幅広いスタイルでお使いいただけます。",
+  category_id: 2,
+  color_id: 12,
+  size_id: 3,
+  price: 6990
+  )
+item_1.image.attach(io: File.open(Rails.root.join("./app/assets/images/sample1.jpeg")), filename: 'sample1.jpeg')
+item_1.save
+
+item_2 = Item.new(
+  name: "ジーンズ",
+  description: "ストレッチ抜群の履き心地にこだわったジーンズ。",
+  category_id: 2,
+  color_id: 8,
+  size_id: 3,
+  price: 12900
+  )
+item_2.image.attach(io: File.open(Rails.root.join("./app/assets/images/sample2.jpeg")), filename: 'sample2.jpeg')
+item_2.save
+
+item_3 = Item.new(
+  name: "リング",
+  description: "スタイリングのプラス1として最適",
+  category_id: 5,
+  color_id: 13,
+  size_id: 5,
+  price: 8900
+  )
+item_3.image.attach(io: File.open(Rails.root.join("./app/assets/images/sample3.jpeg")), filename: 'sample3.jpeg')
+item_3.save
+
+
+
