1232919aa05e797bfac3a18a4209269d026916bd
jtghjt <junjunjun.t.13@gmail.com>
2022-04-01 18:48:03 +0900

    バリデーション設定

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,4 +8,15 @@ class Customer < ApplicationRecord
   has_many :favorites
   has_many :carts
   has_many :comments
+
+  with_options presence: true do
+    validates :nickname
+    VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+    validates :password, format: { :with VALID_PASSWORD_REGEX, message: 'Include both letters and numbers' }
+    validates :last_name, format: { :with /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+    validates :first_name, format: { :with /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+    validates :last_name_kana, format: { :with /\A[ァ-ヶー－]+\z/ }
+    validates :first_name_kana, format: { :with /\A[ァ-ヶー－]+\z/ }
+    validates :brithday
+  end
 end
