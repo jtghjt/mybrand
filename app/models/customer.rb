b0d2f3965342b459aee75f02ead6bf49a156f579
jtghjt <junjunjun.t.13@gmail.com>
2022-04-01 18:32:31 +0900

    新規登録、ログインのビュー作成

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,9 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :orders
+  has_many :favorites
+  has_many :carts
+  has_many :comments
 end
