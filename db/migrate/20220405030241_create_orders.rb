25881f26e78a4180c444e2fea65b521eba6153d2
jtghjt <junjunjun.t.13@gmail.com>
2022-04-06 12:24:29 +0900

    購入機能の修正

--- db/migrate/20220405030241_create_orders.rb
+++ db/migrate/20220405030241_create_orders.rb
@@ -1,8 +1,6 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
-      t.integer    :quantity_id, null:false
-      t.integer    :total_price, null:false
       t.references :customer, null:false, foreign_key: true
       t.references :item, null:false, foreign_kry: true 
       t.timestamps
