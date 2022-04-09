4adc11f03385cb0133781268b1244894983fdd50
jtghjt <junjunjun.t.13@gmail.com>
2022-04-10 02:14:42 +0900

    コメントアクションの設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to product_path[:product_id]
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment).merge(customer_id: current_customer.id, product_id: params[:product_id])
+  end
 end
