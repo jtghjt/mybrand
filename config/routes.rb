cc869f3860f24e55bd81f51475913a6c5fd0ac49
jtghjt <junjunjun.t.13@gmail.com>
2022-04-11 14:16:36 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :customers
   root to: "items#index"
   resources :items do
+    collection do
+      get 'search'
+    end
     resources :orders, only: [:index, :create]
   end
   resources :products, only: [:index, :show] do
