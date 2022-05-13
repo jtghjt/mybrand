8351bd8fcdb0c4a1e555ac1d1a3e019109b3a3b6
jtghjt <junjunjun.t.13@gmail.com>
2022-05-13 13:51:32 +0900

    update機能実装

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,11 +2,25 @@ class CustomersController < ApplicationController
   before_action :authenticate_customer!
 
   def show
-    @nickname = current_customer.nickname
     @favorites = Favorite.where(customer_id: current_customer.id)
   end
 
   def edit
-    @customer = current_customer
   end
+
+  def update
+    if current_customer.update(customer_params)
+      bypass_sign_in(current_customer)
+      redirect_to customer_path(current_customer.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:nickname, :email, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday)
+  end
+
 end
